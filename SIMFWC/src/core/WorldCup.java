89ab0a0f5b286a0b09d1f5b21c64a086cbee195f
labadi0 <47151390+labadi0@users.noreply.github.com>
2020-01-24 16:24:08 +0100

    Add files via upload

---
+++ SIMFWC/src/core/WorldCup.java
@@ -0,0 +1,50 @@
+
+package core;
+import data.Player;
+import data.Team;
+import data.Coach;
+
+public class WorldCup {
+
+	public static void main(String[] args) {
+//(String name, String firstName, int age, int weight, int height, String position, int speed, int center, int pass, int header, int shoot, int energy, int power, int tackle) {
+
+		// TODO Auto-generated method stub
+		Player b1 = new Player("kaka", "recardo", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b2 = new Player("robino", "tchiko", 29, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b3 = new Player("ronolado", "dalima", 25, 60, 170, "att", 90, 80, 90, 99, 99, 89,99, 99);
+		Player b4 = new Player("alves", "daniel", 25, 60, 170, "att", 90, 90, 90, 70, 90, 89, 88, 50);
+		Player b5 = new Player("adriono", "danielo", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b6 = new Player("allison", "teo", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b7 = new Player("dasilva", "santos", 25, 60, 170, "def", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b8 = new Player("ronaldinio", "kafo", 25, 60, 170, "def", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b9 = new Player("pele", "teo", 25, 60, 170, "def", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b10 = new Player("cotenio", "philipe", 25, 60, 170, "def", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player b11 = new Player("roberto", "roberto", 25, 60, 170, "def", 90, 80, 90, 70, 90, 89, 88, 50);
+		
+		
+		
+		
+		
+		
+		Player f1 = new Player("mbape", "kilien", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f2 = new Player("giroud", "antoine", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f3 = new Player("grizman", "antoine", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f4 = new Player("paul", "pogba", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f5 = new Player("ngolo", "kante", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f6 = new Player("lucas", "vaskes", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f7 = new Player("zizo", "zidan", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f8 = new Player("theryy", "hanry", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f9 = new Player("samuel", "umtiti", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f10 = new Player("payet", "dimitri", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		Player f11 = new Player("kaka", "recardo", 25, 60, 170, "att", 90, 80, 90, 70, 90, 89, 88, 50);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
